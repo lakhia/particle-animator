82b55c4da48b6a23ad3f8af5c5894748d8762c4e
Ali Lakhia <lakhia@gmail.com>
2024-09-07 11:10:00 -0700

    Use emitter loop to avoid duplicate code Use stars for symbols, use orange color

--- Emitter.py
+++ Emitter.py
@@ -5,11 +5,12 @@ from ParticleCollector import ParticleCollector, Node
 
 
 class Emitter(Node):
-    def __init__(self, rate, angle_variation=10.0,
+    def __init__(self, rate, angle_variation=10.0, par_angle_offset=0.0,
                  speed=65.0, speed_variation=35.0, **kwargs):
         super().__init__(**kwargs)
         self.rate = rate
         self.angle_variation = angle_variation
+        self.par_angle_offset = par_angle_offset
         self.speed_min = speed - speed_variation
         self.speed_max = speed + speed_variation
         self.collector = ParticleCollector()
@@ -18,12 +19,12 @@ class Emitter(Node):
         super().run()
         self.angle += 3
         for _ in range(int(self.rate)):
-            hypo = random.uniform(self.speed_min, self.speed_max)
-            angle_dev = random.normalvariate(0, self.angle_variation)
+            hypo = random.uniform(self.speed_min, self.speed_max) / 1000
+            angle_dev = self.par_angle_offset + random.normalvariate(0, self.angle_variation)
             angle = math.radians(self.angle + angle_dev)
-            vx = math.cos(angle) / hypo
-            vy = math.sin(angle) / hypo
-            sz = random.uniform(10, 28)
+            vx = math.cos(angle) * hypo
+            vy = math.sin(angle) * hypo
+            sz = random.uniform(4, 15)
             self.collector.add_particle(px=self.px, py=self.py, vx=vx, vy=vy,
                                         size=sz, color=angle_dev / 30)
 
--- particle_animator.py
+++ particle_animator.py
@@ -4,77 +4,14 @@ import numpy as np
 from Emitter import Emitter
 
 np.random.seed(1)
-N = 5
 FACTOR = 4
-emitter1 = Emitter(18, thrust=0.002, angle=-135, px=0.5, py=0.5)
-emitter2 = Emitter(18, thrust=0.002, angle=135, px=0.5, py=0.5)
-emitter3 = Emitter(18, thrust=0.002, angle=-45, px=0.5, py=0.5)
-emitter4 = Emitter(18, thrust=0.002, angle=45, px=0.5, py=0.5)
+emitters = [Emitter(3, thrust=0.01, angle=90, px=0.75, py=0.5,
+                    par_angle_offset=-90, speed=3, speed_variation=2),
+            Emitter(3, thrust=0.01, angle=90, px=0.75, py=0.5,
+                    par_angle_offset=90, speed=3, speed_variation=2)]
 
-for n in range(1, 240):
-    emitter1.run()
-    emitter2.run()
-    emitter3.run()
-    emitter4.run()
-
+for n in range(1, 300):
     fig = go.Figure()
-    fig.add_trace(go.Scatter(
-        x=emitter1.collector.px,
-        y=emitter1.collector.py,
-        mode="markers",
-        marker=go.scatter.Marker(
-            size=emitter1.collector.sz * FACTOR,
-            color=emitter1.collector.cl,
-            opacity=0.6,
-            line=dict(
-                width=0
-            ),
-            colorscale="Viridis"
-        )
-    ))
-    fig.add_trace(go.Scatter(
-        x=emitter2.collector.px,
-        y=emitter2.collector.py,
-        mode="markers",
-        marker=go.scatter.Marker(
-            size=emitter2.collector.sz * FACTOR,
-            color=emitter2.collector.cl,
-            opacity=0.6,
-            line=dict(
-                width=0
-            ),
-            colorscale="Viridis"
-        )
-    ))
-    fig.add_trace(go.Scatter(
-        x=emitter3.collector.px,
-        y=emitter3.collector.py,
-        mode="markers",
-        marker=go.scatter.Marker(
-            size=emitter3.collector.sz * FACTOR,
-            color=emitter3.collector.cl,
-            opacity=0.6,
-            line=dict(
-                width=0
-            ),
-            colorscale="Viridis"
-        )
-    ))
-    fig.add_trace(go.Scatter(
-        x=emitter4.collector.px,
-        y=emitter4.collector.py,
-        mode="markers",
-        marker=go.scatter.Marker(
-            size=emitter4.collector.sz * FACTOR,
-            color=emitter4.collector.cl,
-            opacity=0.6,
-            line=dict(
-                width=0
-            ),
-            colorscale="Viridis"
-        )
-    ))
-
     fig.update_xaxes(visible=False, zeroline=False, showgrid=False, range=[0, 1])
     fig.update_yaxes(visible=False, zeroline=False, showgrid=False, range=[0, 1])
     fig.update_layout(
@@ -84,6 +21,25 @@ for n in range(1, 240):
         height=540 * FACTOR,
         margin=dict(l=0, r=0, t=0, b=0)
     )
+
+    for emitter in emitters:
+        emitter.run()
+        fig.add_trace(go.Scatter(
+            x=emitter.collector.px,
+            y=emitter.collector.py,
+            mode="markers",
+            marker=go.scatter.Marker(
+                size=emitter.collector.sz * FACTOR,
+                color=emitter.collector.cl,
+                symbol='star',
+                opacity=0.6,
+                line=dict(
+                    width=0
+                ),
+                colorscale="oranges"
+            )
+        ))
+
     if n % 10 == 0:
         print("Wrote %d frame" % n)
 
